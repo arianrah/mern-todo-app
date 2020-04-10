70a863a3c454bd4c88616f8180df0a90fd45f3af
arianrah <arianrahimi@icloud.com>
2020-04-09 19:50:49 -0700

    Base component structure.

---
+++ src/components/Create-Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateTodo extends Component {
+  render() {
+    return (
+      <div>
+        <p>Create-Todo.js (Component)</p>
+      </div>
+    );
+  }
+}
