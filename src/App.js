bc69c7f4076d42e4025d51ec10a4d6eed745974a
arianrah <arianrahimi@icloud.com>
2020-04-10 18:03:49 -0700

    Added bootstrap nav bar, +example logo placeholder (react logo)

--- src/App.js
+++ src/App.js
@@ -6,13 +6,44 @@ import CreateTodo from "./components/Create-Todo";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import logo from "./logo/logo512.png";
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              alt="React Logo (example image)"
+            />
+
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br />
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
       </Router>
     );
   }
